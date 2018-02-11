2526701a4491e5a9fe4876e1434ebe603816ebd0
Ben <bakerb845@gmail.com>
2018-02-11 15:53:17 -0800

    moving towards a first release

--- python/fteik.py
+++ python/fteik.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Python interface to the fteik eikonal equation Fortran library.
+
+Copyright: Ben Baker distributed under the MIT license.
+"""
 import ctypes
 from ctypes import c_int
 from ctypes import c_double
@@ -81,6 +86,7 @@ class fteik2d:
         """
         Frees the internal Fortran module.
         """
+        self.linit = False
         self.nx = 0
         self.nz = 0
         self.nsrc = 0
@@ -91,8 +97,38 @@ class fteik2d:
                    x0 = 0.0, z0 = 0.0,
                    nsweep = 2, eps = 3.0, verbose=0):
         """
-        Initializes the 2D solver geometry.
+        Initializes the 2D solver eikonal solver.
 
+        Required Arguments 
+        nx : int
+           Number of x grid points in the travel time field.
+           This must be at least 3.
+        nz : int
+           Number of z grid points in the travel time field. 
+           This must be at least 3. 
+        dx : float
+           Grid spacing (meters) in x.  This cannot be 0.0.
+        dz : float
+           Grid spacing (meters) in z.  This cannot be 0.0.
+
+        Optional Arguments 
+        x0 : float
+           Model origin in x (meters). 
+        z0 : float
+           Model origin in z (meters).
+        nsweep : int
+           Number of Gauss-Seidel iterations.
+        eps : float
+           Number of grid-points around source where, on initialization, the
+           the solver switches from a spherical solver to a cartesian solver.
+           This variable can be thought of in the sense that the eikonal 
+           equation assumes a plane wave solution to the acoustic wave equation.
+           In the source region there will be high-curvature which is difficult
+           to approximate in cartesian coordinates.
+
+        verbose : int
+           Controls verbosity where 0 is quiet and 1, 2, ... correspond to
+           an increasing number of messages from the module to standard out.
         """
         ierr = c_int(1)
         self.fteik2d.fteik_solver2d_initialize64f(nz, nx,
@@ -169,6 +205,115 @@ class fteik2d:
     def setReceivers(self, xrec, zrec):
         return 
 
+################################################################################
+#                                 3D Class                                     #
+################################################################################
+
+class fteik3d:
+    def __init__(self,
+                 fteik_library='/usr/local/lib/libfteik_shared.so'):
+        lib = ctypes.cdll.LoadLibrary(fteik_library)
+        # Make the interfaces
+        lib.fteik_solver3d_initialize64f.argtypes = (c_int,     #nz 
+                                                     c_int,     #nx 
+                                                     c_int,     #ny
+                                                     c_double,  #z0 
+                                                     c_double,  #x0 
+                                                     c_double,  #y0
+                                                     c_double,  #dz 
+                                                     c_double,  #dx 
+                                                     c_double,  #dz
+                                                     c_int,     #nsweeps
+                                                     c_double,  #eps
+                                                     c_int,     #verbosity
+                                                     POINTER(c_int) #ierr
+                                                    )
+        lib.fteik_solver3d_setVelocityModel64f.argtypes = (c_int,             #ncell
+                                                           POINTER(c_double), #velocity
+                                                           POINTER(c_int)     #ierr
+                                                          )
+
+        lib.fteik_solver3d_free.argtypes = None
+        self.linit = False
+        self.nx = 0
+        self.ny = 0
+        self.nz = 0
+        self.nsrc = 0
+        self.fteik3d = lib
+        return
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        self.fteik3d.fteik_solver3d_free()
+        return
+
+    def free(self):
+        """
+        Frees the internal Fortran module.
+        """
+        self.nx = 0
+        self.ny = 0
+        self.nz = 0
+        self.nsrc = 0
+        self.linit = False
+        self.fteik3d.fteik_solver3d_free()
+        return
+
+    def initialize(self, nx, ny, nz, dx, dy, dz,
+                   x0 = 0.0, y0 = 0.0, z0 = 0.0,
+                   nsweep = 2, eps = 3.0, verbose=0):
+        """
+        Initializes the 3D eikonal solver.  This will trigger a graph ordering 
+        operation which can be fairly expensive and should only be done once.
+
+        Required Arguments 
+        nx : int
+           Number of x grid points in the travel time field.
+           This must be at least 3.
+        nz : int
+           Number of z grid points in the travel time field. 
+           This must be at least 3. 
+        dx : float
+           Grid spacing (meters) in x.  This cannot be 0.0.
+        dz : float
+           Grid spacing (meters) in z.  This cannot be 0.0.
+
+        Optional Arguments 
+        x0 : float
+           Model origin in x (meters). 
+        z0 : float
+           Model origin in z (meters).
+        nsweep : int
+           Number of Gauss-Seidel iterations.
+        eps : float
+           Number of grid-points around source where, on initialization, the
+           the solver switches from a spherical solver to a cartesian solver.
+           This variable can be thought of in the sense that the eikonal 
+           equation assumes a plane wave solution to the acoustic wave equation.
+           In the source region there will be high-curvature which is difficult
+           to approximate in cartesian coordinates.
+
+        verbose : int
+           Controls verbosity where 0 is quiet and 1, 2, ... correspond to
+           an increasing number of messages from the module to standard out.
+        """
+        ierr = c_int(1)
+        self.fteik3d.fteik_solver3d_initialize64f(nz, nx, ny,
+                                                  z0, x0, y0,
+                                                  dz, dx, dy,
+                                                  nsweep, eps,
+                                                  verbose, byref(ierr))
+        if (ierr.value != 0):
+            print("Error initializing solver")
+            return -1
+        self.nx = nx
+        self.ny = ny
+        self.nz = nz
+        return 0
+
+
 if __name__ == "__main__":
     nx = 101
     nz = 101
@@ -187,6 +332,7 @@ if __name__ == "__main__":
     ttimes = fteik2d.getTravelTimeField()
     fteik2d.free()
     # plot it
+    """
     from matplotlib import pyplot as plt
     plt.contourf(x, z, ttimes, 25, cmap=plt.cm.viridis)
     plt.ylim((nz - 1)*dz, 0)
@@ -195,3 +341,12 @@ if __name__ == "__main__":
     plt.ylabel('Depth (m)')
     plt.colorbar()
     plt.show()
+    """
+    nx = 51
+    ny = 51
+    nz = 25 
+    dx = 10.0
+    dy = 10.0
+    dz = 10.0
+    fteik3d = fteik3d('/home/bakerb25/C/fteik/lib/libfteik_shared.so')
+    fteik3d.initialize(nx, ny, nz, dx, dy, dz, verbose=3) 
